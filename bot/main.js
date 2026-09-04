f99c1ec424eaead0498d21dc1208732d53e4bc7b
Zed <lordjanx2025@gmail.com>
2026-09-04 06:48:44 +0300

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -9,7 +9,7 @@ const msgCounters = new Map();
 const REACTION_EMOJIS = ["🖤", "🥒", "☠️", "💀", "🔥"];
 const REACTION_MILESTONE = 568;
 
-const BOT_NICKNAME = ` ⏤͟͟ 卍 ୨ৎ⃝ ˖ 🇦🇱 𝆺𝅥⃝𝗭𝗘𝗗 ;
+const BOT_NICKNAME = ` ⏤͟͟ 卍 ୨ৎ⃝ ˖ 🇦🇱 𝆺𝅥⃝𝗭𝗘𝗗` ;
 
 function isAdmin(senderID) {
   return ADMINS.has(String(senderID));
